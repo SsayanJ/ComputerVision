a3d311b616b045dbc948bf00404b3e8caf48df5e
Jeff <jean-francois.benhaim@epita.fr>
2021-05-22 14:16:07 +0200

    Create function to undistort image

--- Undistort_images.py
+++ Undistort_images.py
@@ -3,20 +3,26 @@ import cv2 as cv
 import glob
 import json
 
-# Recover fisheye camera settings from JSON config file
-with open('Fisheye_config.json') as config:
-    fisheye_cfg=json.load(config)
 
-mtx=fisheye_cfg['mtx']
-dist=fisheye_cfg['dist']
+def undistort_image(image):
+    # Recover fisheye camera settings from JSON config file
+    with open('Fisheye_config.json') as config:
+        fisheye_cfg=json.load(config)
 
-mtx=np.asarray(mtx)
-dist=np.asarray(dist)
-DIM= (1280,720)  # standard dimension of pics from camera
+    mtx=fisheye_cfg['mtx']
+    dist=fisheye_cfg['dist']
 
-img = cv.imread('ongoing_calib/board_img_22.png')
+    mtx=np.asarray(mtx)
+    dist=np.asarray(dist)
+    DIM= (1280,720)  # standard dimension of pics from camera
 
-map1,map2=cv.fisheye.initUndistortRectifyMap(mtx,dist,np.eye(3),mtx,DIM,cv.CV_16SC2)
-dst=cv.remap(img, map1, map2, interpolation=cv.INTER_LINEAR, borderMode=cv.BORDER_CONSTANT)
+    img = cv.imread(filename)
 
-cv.imwrite('calibresult2.png', dst)
+    map1,map2=cv.fisheye.initUndistortRectifyMap(mtx,dist,np.eye(3),mtx,DIM,cv.CV_16SC2)
+    dst=cv.remap(img, map1, map2, interpolation=cv.INTER_LINEAR, borderMode=cv.BORDER_CONSTANT)
+
+    return cv.imwrite('calibresult2.png', dst)
+
+filename = "test_WA.png"
+# filename = 'ongoing_calib/board_img_22.png'
+undistort_image(filename)
